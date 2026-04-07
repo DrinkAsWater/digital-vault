1ee2d9354214bf938094ad2c31aca5a32e7ea509
DrinkAsWater <gtasa12500@gmail.com>
2026-04-07 20:48:07 +0800

    修改

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
-      "/api": {
-        target: "https://localhost:7124",
+      '/api': {
+        target: 'https://localhost:7124',
         changeOrigin: true,
         secure: false,
-      },
-    },
+      }
+    }
   },
-});
+})
